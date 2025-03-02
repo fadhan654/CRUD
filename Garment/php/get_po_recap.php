22d2cc647dde4279849c06b6a59cc60010262005
fadhlan654 <114726913+fadhan654@users.noreply.github.com>
2025-03-03 04:37:02 +0700

    Add files via upload

--- Garment/php/get_po_recap.php
+++ Garment/php/get_po_recap.php
@@ -1,15 +1,33 @@
 <?php
 session_start();
+header('Content-Type: application/json');
+
 $conn = new mysqli('127.0.0.1', 'root', '', 'youngstar_db', 3307);
-
-$sql = "SELECT * FROM po_recap";
-$result = $conn->query($sql);
-
-$data = [];
-while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
+if ($conn->connect_error) {
+    echo json_encode(["status" => "error", "message" => "Database connection failed"]);
+    exit();
 }
 
-echo json_encode(["data" => $data]);
+$source = isset($_GET['source']) ? $_GET['source'] : null;
+if ($source == "walmart") {
+    $sql = "SELECT * FROM po_recap_walmart ORDER BY created_at DESC";
+} elseif ($source == "macys") {
+    $sql = "SELECT * FROM `po_recap_macys` ORDER BY created_at DESC";
+} else {
+    $sql = "SELECT * FROM po_recap ORDER BY created_at DESC"; // Semua Data
+}
+
+$result = $conn->query($sql);
+$data = [];
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode(["data" => $data]);
+} else {
+    echo json_encode(["status" => "error", "message" => "Query failed"]);
+}
+
 $conn->close();
 ?>
